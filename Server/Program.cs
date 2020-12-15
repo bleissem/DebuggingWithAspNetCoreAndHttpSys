3c48f23f95504efe7833e378b880ab8a54315d23
Alexander Bleissem <alexander@bleissem.com>
2020-12-15 16:01:26 +0100

    breakpoint does not get hit when using httpsys

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,26 +1,40 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.HttpSys;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Threading.Tasks;
 
 namespace DebuggingWithAspNetCoreAndHttpSys.Server
 {
     public class Program
     {
+        [SupportedOSPlatform("windows")]
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
+
+        [SupportedOSPlatform("windows")]
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    // enable httpsys to be able to use Windows authentication
+                    webBuilder.UseHttpSys(options =>
+                    {
+                        options.Authentication.Schemes =
+                            AuthenticationSchemes.NTLM |
+                            AuthenticationSchemes.Negotiate;
+                        options.Authentication.AllowAnonymous = false;
+                        options.AllowSynchronousIO = true;
+                    });
                 });
     }
 }
